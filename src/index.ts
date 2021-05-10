e4e9cb9b5272bcc219bb62d91ee1cdbcb25a94b6
corrinalanis <32191211+corrinalanis@users.noreply.github.com>
2021-05-09 23:56:09 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -57,3 +57,97 @@ function resizeCanvas(): void {
 
   window.addEventListener("resize", resize);
 }
+
+function setup() {
+  console.log("sounds loaded");
+
+  //Create the sounds
+  var shoot = sounds["sounds/shoot.wav"],
+      music = sounds["sounds/music.wav"],
+      bounce = sounds["sounds/bounce.mp3"];
+
+  //Pan the shoot sound to the right
+  shoot.pan = 0.8;
+
+  //Make the music loop
+  music.loop = true;
+
+  //Set the pan to the left
+  music.pan = -0.8;
+
+  //Set the music volume
+  music.volume = 0.7;  
+
+  //Set a reverb effect on the bounce sound
+  //arguments: duration, decay, reverse?
+  //music.setReverb(2, 2, false);
+
+  //Set the sound's `reverb` property to `false` to turn it off
+  //music.reverb = false;
+
+  //Add an echo effect to the bounce sound
+  //arguments: delay time, feedback time, optional frequency filtering
+  bounce.setEcho(0.2, 0.3, 1000);
+
+  //Set `echo` to false to turn it off
+  //bounce.echo = false;
+  
+  //Optionally set the music playback rate to half speed
+  //music.playbackRate = 0.5;
+
+  //Capture the keyboard events
+  var a = keyboard(65),
+      b = keyboard(66),
+      c = keyboard(67),
+      d = keyboard(68),
+      e = keyboard(69),
+      f = keyboard(70);
+      g = keyboard(71);
+      h = keyboard(72);
+
+  //Control the sounds based on which keys are pressed
+
+  //Play the loaded shoot sound
+  a.press = function() { shoot.play() };
+
+  //Play the loaded music sound
+  b.press = function() {
+    if (!music.playing) {
+      music.play();
+    }
+    console.log("music playing");
+  };
+
+  //Pause the music 
+  c.press = function() {
+    music.pause();
+    console.log("music paused");
+  };
+
+  //Restart the music 
+  d.press = function() {
+    music.restart();
+    console.log("music restarted");
+  };
+
+  //Play the music from the 10 second mark
+  e.press = function() {
+    music.playFrom(10);
+    console.log("music start point changed");
+  };
+  
+  //Play the bounce sound
+  f.press = function() { bounce.play() };
+
+  //Fade the music out over 3 seconds
+  g.press = function() { 
+    music.fadeOut(3);
+  };
+
+  //Fade the music in over 3 seconds
+  h.press = function() { 
+    music.fadeIn(3);
+  };
+}
+
+
