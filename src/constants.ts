307bf15f24a93d8ad19c3eb990c2e0cefe37ac6f
yazenelhamad <60762842+yazenelhamad@users.noreply.github.com>
2021-05-10 01:25:33 -0500

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,35 @@ export const TEXTURES = {
   PLATFORM: "assets/platform/Tileset.json",
   PLAYER: "assets/characters/npc.json",
 } as const;
+
+export const Scene = {
+  Width: 1280,
+  Height: 578
+};
+
+export const KeyCodes = {
+  Space: "Space",
+  ArrowLeft: "ArrowLeft",
+  ArrowRight: "ArrowRight"
+};
+
+export const World = {
+  Character: {
+    Speed: 2,
+    JumpSpeed: 3,
+    JumpThreshold: 50
+  },
+  Gravity: 3
+};
+
+export const CharacterDirections = {
+  Left: -1,
+  Right: 1
+};
+
+export enum CharacterMode {
+  Jumping,
+  Running,
+  Falling,
+  Idle
+}
